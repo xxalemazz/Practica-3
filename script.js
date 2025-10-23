a9769b1195d7755045ac4de2fbb3838965be4ae0
Emiliano <emiliano@gmail.com>
2025-10-22 18:35:58 -0600

    Agrego estilos

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ async function Api() {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
     const Datos = await res.json();
 
-    document.querySelector(".Nombre").textContent = Datos.name.toUpperCase();
+    
+    const nombreDiv = document.querySelector(".Nombre");
+    nombreDiv.innerHTML = `<h2>${Datos.name.toUpperCase()}</h2>`;
+    if(Datos.sprites.front_default){
+        const img = document.createElement("img");
+        img.src = Datos.sprites.front_default;
+        nombreDiv.appendChild(img);
+    }
 
+    
     document.querySelector(".Estadisticas").innerHTML = `
+        <h3>Estadísticas:</h3>
         <p>Peso: ${Datos.weight}</p>
         <p>Altura: ${Datos.height}</p>
         <p>Base Experience: ${Datos.base_experience}</p>
     `;
 
+    
     const habilidadesDiv = document.querySelector(".Habilidades");
     habilidadesDiv.innerHTML = "<h3>Habilidades:</h3>";
     Datos.abilities.forEach(a => {
@@ -18,6 +28,7 @@ async function Api() {
         habilidadesDiv.appendChild(p);
     });
 
+    
     const objetosDiv = document.querySelector(".objetos");
     objetosDiv.innerHTML = "<h3>Objetos:</h3>";
     if(Datos.held_items.length === 0){
@@ -30,13 +41,6 @@ async function Api() {
         });
     }
 
-    const tiposDiv = document.querySelector(".Tipos");
-    tiposDiv.innerHTML = "<h3>Tipos:</h3>";
-    Datos.types.forEach(tipo => {
-        const p = document.createElement("p");
-        p.textContent = tipo.type.name;
-        tiposDiv.appendChild(p);
-    });
 
     const movimientosDiv = document.querySelector(".Movimientos");
     movimientosDiv.innerHTML = "<h3>Movimientos:</h3>";
@@ -46,8 +50,20 @@ async function Api() {
         movimientosDiv.appendChild(p);
     });
 
+
+    const tiposDiv = document.querySelector(".Tipos");
+    tiposDiv.innerHTML = "<h3>Tipos:</h3>";
+    Datos.types.forEach(tipo => {
+        const p = document.createElement("p");
+        p.textContent = tipo.type.name;
+        tiposDiv.appendChild(p);
+    });
+
+    
     const enlacesDiv = document.querySelector(".enlaces");
-    enlacesDiv.innerHTML = `<p>Especie URL: <a href="${Datos.species.url}" target="_blank">${Datos.species.url}</a></p>`;
+    enlacesDiv.innerHTML = `<h3>Enlaces:</h3>
+                            <p>Especie URL: <a href="${Datos.species.url}" target="_blank">${Datos.species.url}</a></p>`;
 }
+
 
 document.addEventListener("DOMContentLoaded", Api);
